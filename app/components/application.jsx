36e955945518e545bb18380382dd27d8cef0f455
darul75 <darul75@gmail.com>
2015-04-24 10:03:24 +0200

    fl

--- app/components/application.jsx
+++ app/components/application.jsx
@@ -1,9 +1,25 @@
-import React from 'react';
-import { RouteHandler } from "react-router";
+import React from 'react'
+import { RouteHandler } from "react-router"
+import TodoStore from '../stores/TodoStore'
 
-import Header from './header';
+import Header from './header'
+
+/**
+ * Retrieve the current TODO data from the TodoStore
+ */
+function getTodoState() {
+  return {
+    allTodos: TodoStore.getState().todos,
+    areAllComplete: TodoStore.areAllComplete()
+  };
+}
+
+console.log(getTodoState());
 
 export default class Application extends React.Component {  
+  getInitialState() {    
+    return getTodoState();
+  }
   render() {
     return (
       <div>
--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Route, DefaultRoute, NotFoundRoute} from 'react-router';
 
-import Application from './components/application';
-import Home from './components/home';
-import Contact from './components/contact';
+import Application from './components/Application';
+import Home from './components/Home';
+import Contact from './components/Contact';
 
 export default (
   <Route name="app" path="/" handler={Application}>
