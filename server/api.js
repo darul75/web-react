7455de67fdd70fd2e458aba462a621ddf02c4fec
Julien Valéry <darul75@gmail.com>
2015-06-11 16:50:48 +0200

    |

--- server/api.js
+++ server/api.js
@@ -1,23 +1,28 @@
-import fetch from 'node-fetch';
-
-function query(server) {
-  server.get('/api/*', (req, res, next) => {
+let api = {
+  todo: (req, res, next) => {
     try {
       res.setHeader('Content-Type', 'application/json');
 
       // simple api fetch example, no own DB here so external call is made.
 
       // https://api.github.com/users/github
-      fetch('https://api.github.com/users/github')
+     /* fetch('https://api.github.com/users/github')
       .then((response) => {
         return response.json();
       }).then((json) => {
         res.status(200).send(json);
       });
+      */
+      res.status(200).send({test: 'testdd'});
     } catch (err) {
       next(err);
     }
-  });
-}
+  },
+  othertodo: () => {
 
-export default query;
+  }
+};
+
+export default api;
+
+
